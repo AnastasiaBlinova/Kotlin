ea6b5c1b4c5f85462a64e378702f12840aa1a7f6
Nastya <nastya.com107@gmail.com>
2024-02-27 06:51:00 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,39 @@
 package org.example
 
-import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.isActive
 import kotlin.random.Random
 
 fun main() {
+    // Получим значения из флоу
+    runBlocking {
+        launch {
+            println("Start collecting...")
+            Generator.flow.collect {
+                // в блоке collect указываем код, который будет
+                // выполнятся при получении каждого из значений flow
+                println("Collected value - $it")
+            }
+            println("Start working...")
+        }
+    }
+
 }
 object Generator {    //создали поток чисел где каждую секунду размещаются случайные числа
     val flow = flow {
-        while (currentCoroutineContext().isActive)
-        emit(Random.nextInt())
-        delay(1000)
+        while (currentCoroutineContext().isActive) {
+            emit(Random.nextInt())
+            delay(1000)
+        }
     }
     fun flow(): Flow<Int> {
         return flow {
-            while (currentCoroutineContext().isActive)
+            while (currentCoroutineContext().isActive) {
                 emit(Random.nextInt())
-            delay(1000)
+                delay(1000)
+            }
         }
     }
 }
