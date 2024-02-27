159057c90aae38672dcc1f720bcc48a93f6904ee
Nastya <nastya.com107@gmail.com>
2024-02-27 06:43:05 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,25 @@
 package org.example
 
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.isActive
+import kotlin.random.Random
+
 fun main() {
-    println("Hello World!")
+}
+object Generator {    //создали поток чисел где каждую секунду размещаются случайные числа
+    val flow = flow {
+        while (currentCoroutineContext().isActive)
+        emit(Random.nextInt())
+        delay(1000)
+    }
+    fun flow(): Flow<Int> {
+        return flow {
+            while (currentCoroutineContext().isActive)
+                emit(Random.nextInt())
+            delay(1000)
+        }
+    }
 }
